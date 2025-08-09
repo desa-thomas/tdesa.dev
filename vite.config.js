aead8be287d459bca0e6dbca0a1bba2ae791e860
tom <desa2thomas@gmail.com>
2025-08-09 18:09:08 -0400

    Minor style changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/', 
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true,        // <-- Allows access from network (0.0.0.0)
     port: 5500,        
